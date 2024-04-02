b21232bbca7ea36cc21cdba1002f640e4196133f
Gowtham Kaliraj <gowtham.kaliraj@appviewx.com>
2024-04-02 10:15:21 +0530

    data ingestion notebook added

--- template.py
+++ template.py
@@ -10,7 +10,7 @@ project_name = "textSummarizer"
 list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/conponents/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
     f"src/{project_name}/logging/__init__.py",
@@ -26,24 +26,27 @@ list_of_files = [
     "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "research/trails.ipynb"
+    "research/trials.ipynb",
+
 ]
 
 
-for file_path in list_of_files:
-    filepath = Path(file_path)
+for filepath in list_of_files:
+    filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
-    
-    if filedir:
+
+    if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for thr file: {filename}")
-        
-    if (
-        not os.path.exists(filepath)
-        or os.path.getsize(filepath) == 0
-    ):
-        with open(filepath, 'w') as f:
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
+
+
+    
     else:
-        logging.info(f"{filename} is already exists.")
+        logging.info(f"{filename} is already exists")
+
